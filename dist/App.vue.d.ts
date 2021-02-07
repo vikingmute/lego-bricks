7f488507bd048aa26c8e30fc304e84d0b2c968a9
Viking Zhang <vikingmute@gmail.com>
2021-02-07 16:21:14 +0800

    test rollup script for the index.ts, add node-resolve-plugin and external

--- dist/App.vue.d.ts
+++
@@ -1,5 +0,0 @@
-export interface TestProps {
-    name: string;
-}
-declare const _default: import("vue").DefineComponent<{}, {}, {}, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, import("vue").EmitsOptions, string, import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps, Readonly<{} & {}>, {}>;
-export default _default;
